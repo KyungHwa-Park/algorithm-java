0bef875a1b9a4d38e85261ddf588ad1149315935
KyungHwa Park <kyunghwa.park123@gmail.com>
2019-01-29 18:35:22 +0900

    거꾸로 출력하기
    SW Expert 1545.

---
+++ sw_expert_solution/src/difficulty1/InvertedPrint.java
@@ -0,0 +1,18 @@
+package difficulty1;
+
+import java.util.Scanner;
+
+public class InvertedPrint {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for (int i = n; i >= 0; i--) {
+			System.out.print(i + " ");
+		}
+
+	}
+
+}
