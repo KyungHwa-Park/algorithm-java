567004c25c8fd157e6aaed5376dc03e790904596
KyungHwa Park <kyunghwa.park123@gmail.com>
2019-01-29 20:37:40 +0900

    스탬프 찍기
    SW Expert 2046.

---
+++ sw_expert_solution/src/difficulty1/Stamp.java
@@ -0,0 +1,18 @@
+package difficulty1;
+
+import java.util.Scanner;
+
+public class Stamp {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			System.out.print("#");
+		}
+
+	}
+
+}
