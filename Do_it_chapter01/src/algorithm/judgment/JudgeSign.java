d3caf4488f85cb966b5f8560f9ad09e86d6c7538
KyungHwa Park <kyunghwa.park123@gmail.com>
2019-02-15 19:15:06 +0900

    조건 판단과 분기
    01. 기본 알고리즘
    01-1. 알고리즘이란?

---
+++ Do_it_chapter01/src/algorithm/judgment/JudgeSign.java
@@ -0,0 +1,25 @@
+package algorithm.judgment;
+
+import java.util.Scanner;
+
+// 입력한 정숫값이 양수인지 음수인지 0인지 판단합니다.
+public class JudgeSign {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("정수를 입력하세요.");
+		int num = sc.nextInt();
+		
+		if(num > 0) {
+			System.out.println("이 수는 양수입니다.");
+		} else if (num < 0) {
+			System.out.println("이 수는 음수입니다.");
+		} else {
+			System.out.println("이 수는 0입니다.");
+		}
+
+	}
+
+}
