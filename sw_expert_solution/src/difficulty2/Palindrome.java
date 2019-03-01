d3721e14f2bb49951d3eee9e16c4ad61684d6d05
KyungHwa Park <kyunghwa.park123@gmail.com>
2019-03-02 03:30:11 +0900

    1989. 초심자의 회문 검사
    SW Expert D2.

--- sw_expert_solution/src/difficulty2/Palindrome.java
+++ sw_expert_solution/src/difficulty2/Palindrome.java
@@ -6,24 +6,22 @@ public class Palindrome {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int T = sc.nextInt();
-		for(int tc=1; tc<=T; tc++) {
+		for (int tc = 1; tc <= T; tc++) {
 			String str = sc.next();
 			int i = 0;
-			int j = str.length()-1;
-			
-			for(; i<str.length()/2; i++, j--) {
-				if(str.charAt(i) != str.charAt(j)) {
+			int j = str.length() - 1;
+			int result = 1;
+
+			// 입력받은 str의 길이가 짝수(hannah)일 때, 기존의 코드에서 오류 발생 => 수정 완료!
+			for (; i < str.length()/2; i++, j--) {
+				if (str.charAt(i) != str.charAt(j)) {
+					result = 0;
 					break;
 				}
 			}
-			System.out.print("#" + tc + " ");
-			if(i==j) {
-				System.out.println("1");
-			} else {
-				System.out.println("0");
-			}
+			System.out.printf("#%d %d\n", tc, result);
 		}
 
 	}
